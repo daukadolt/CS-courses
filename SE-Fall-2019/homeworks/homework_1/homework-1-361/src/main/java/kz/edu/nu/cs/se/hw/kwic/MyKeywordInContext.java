1cd4255c87b2e3bc49920c2bae417ad39ae3aefd
daukadolt <daulet.amirkhanov.official@gmail.com>
2019-09-12 00:45:34 +0600

    se-fall-2019: rewrote homework_1 first part

--- SE-Fall-2019/homeworks/homework_1/homework-1-361/src/main/java/kz/edu/nu/cs/se/hw/kwic/MyKeywordInContext.java
+++ SE-Fall-2019/homeworks/homework_1/homework-1-361/src/main/java/kz/edu/nu/cs/se/hw/kwic/MyKeywordInContext.java
@@ -9,6 +9,7 @@ public class MyKeywordInContext implements KeywordInContext {
     private ArrayList<String> txtLines;
     private ArrayList<Indexable> indexes;
     private Map<String, ArrayList<Integer>> wordToIndexable;
+    private String pathToFile;
     private String name;
 
 
@@ -16,6 +17,7 @@ public class MyKeywordInContext implements KeywordInContext {
         KeywordInContext my = new MyKeywordInContext("name", "book.txt");
         my.txt2html();
         my.indexLines();
+        my.writeIndexToFile();
     }
 
     public static <K, V> void printMap(Map<K, V> map) {
@@ -28,9 +30,8 @@ public class MyKeywordInContext implements KeywordInContext {
     public MyKeywordInContext(String name, String pathstring) {
         // TODO Auto-generated constructor stub
         this.name = name;
+        this.pathToFile = pathstring;
         readStopWords();
-        generateLines(pathstring);
-        generateIndexes(pathstring);
     }
 
     private void readStopWords() {
@@ -64,38 +65,6 @@ public class MyKeywordInContext implements KeywordInContext {
         }
     }
 
-    public void generateIndexes(String pathstring) {
-        this.indexes = new ArrayList<>();
-        this.wordToIndexable = new TreeMap<>();
-        int currentLine = 1;
-        for(String line: txtLines) {
-            String[] splittedLine, lineCopy;
-            String word;
-            splittedLine = line.split("[\\p{Punct}\\s]+");
-//            System.out.println("Line #" + currentLine);
-
-            for (int i = 0; i<splittedLine.length; i++) {
-                word = splittedLine[i].toLowerCase();
-                if (stopWords.contains(word) || word.length() == 0) {
-                    continue;
-                }
-                lineCopy = Arrays.copyOf(splittedLine, splittedLine.length);
-                lineCopy[i] = "<a href=\"" + name+ ".html#line_"+currentLine+"\">"+word.toUpperCase()+"</a>";
-//                System.out.println(String.join(" ", lineCopy));
-                Indexable anIndex = new MyIndexable(String.join(" ", lineCopy), currentLine);
-                indexes.add(anIndex);
-                if (!wordToIndexable.containsKey(word)) {
-                    ArrayList<Integer> listOfLines = new ArrayList<>();
-                    listOfLines.add(indexes.size()-1);
-                    wordToIndexable.put(word, listOfLines);
-                } else {
-                    wordToIndexable.get(word).add(indexes.size()-1);
-                }
-            }
-            currentLine++;
-        }
-    }
-
     @Override
     public int find(String word) {
         // TODO Auto-generated method stub
@@ -113,17 +82,21 @@ public class MyKeywordInContext implements KeywordInContext {
     public void txt2html() {
         // TODO Auto-generated method stub
         File anchoredHTML = new File("myKWIC_output/"+this.name+".html");
+        txtLines = new ArrayList<>();
         try {
             anchoredHTML.createNewFile();
             BufferedWriter bw = new BufferedWriter(new FileWriter(anchoredHTML));
             bw.write("<!DOCTYPE html>\n");
             bw.write("<head> <meta charset=\"UTF-8\"> </head>\n");
             bw.write("<div>\n");
+            BufferedReader reader = new BufferedReader(new FileReader(this.pathToFile));
+            String line = reader.readLine();
             int currentLine = 1;
-            for(String line: txtLines) {
+            while(line != null) {
                 bw.write(line);
                 bw.write("<span id=\"line_"+ currentLine +"\">&nbsp&nbsp[" + currentLine + "]</span><br>\n");
                 currentLine++;
+                line = reader.readLine();
             }
             bw.write("</div>\n");
             bw.write("</html>\n");
@@ -136,6 +109,39 @@ public class MyKeywordInContext implements KeywordInContext {
     @Override
     public void indexLines() {
         // TODO Auto-generated method stub
+        generateLines(pathToFile);
+        this.indexes = new ArrayList<>();
+        this.wordToIndexable = new TreeMap<>();
+        int currentLine = 1;
+        for(String line: txtLines) {
+            String[] splittedLine;
+            String word;
+            splittedLine = line.split("[\\p{Punct}\\s]+");
+            for (int i = 0; i<splittedLine.length; i++) {
+                word = splittedLine[i].toLowerCase();
+                if (stopWords.contains(word) || word.length() == 0) {
+                    continue;
+                }
+//                System.out.println(word);
+                if (!wordToIndexable.containsKey(word)) {
+                    Indexable anIndex = new MyIndexable(word, currentLine);
+                    indexes.add(anIndex);
+                    ArrayList<Integer> listOfLines = new ArrayList<>();
+                    listOfLines.add(indexes.size()-1);
+                    wordToIndexable.put(word, listOfLines);
+                } else if( !(this.indexes.get(wordToIndexable.get(word).get(wordToIndexable.get(word).size()-1)).getLineNumber() == currentLine) ) {
+                    Indexable anIndex = new MyIndexable(word, currentLine);
+                    indexes.add(anIndex);
+                    wordToIndexable.get(word).add(indexes.size()-1);
+                }
+            }
+            currentLine++;
+        }
+    }
+
+    @Override
+    public void writeIndexToFile() {
+        // TODO Auto-generated method stub
         File kwicHTML = new File("myKWIC_output/kwic-"+this.name+".html");
         try {
             kwicHTML.createNewFile();
@@ -144,21 +150,27 @@ public class MyKeywordInContext implements KeywordInContext {
             bw.write("<html><head><meta charset=\"UTF-8\"></head><body><div style=\"text-align:center;line-height:1.6\">\n");
             for(Map.Entry <String, ArrayList<Integer>> entry: wordToIndexable.entrySet()) {
                 for(int index: entry.getValue()) {
-                    bw.write(indexes.get(index).getEntry());
+                    Indexable currentIndex = indexes.get(index);
+                    String[] splittedLine = txtLines.get(currentIndex.getLineNumber()-1).split("[\\p{Punct}\\s]+");
+                    for(String word: splittedLine) {
+                        String lowerWord = word.toLowerCase();
+                        if(lowerWord.equals(entry.getKey())) {
+                            bw.write("<a href=\""+this.name+".html#line_"+currentIndex.getLineNumber()+"\">"+lowerWord.toUpperCase()+"</a>");
+                            bw.write(" ");
+                        } else {
+                            bw.write(word);
+                            bw.write(" ");
+                        }
+                    }
                     bw.write("<br>\n");
                 }
             }
+            bw.write("</div>\n");
             bw.write("</html>\n");
             bw.close();
         } catch (IOException e) {
             System.out.println("Failed to create kwicHTML. Reason: " + e.getMessage());
         }
-    }
-
-    @Override
-    public void writeIndexToFile() {
-        // TODO Auto-generated method stub
-
     }
 
 }
