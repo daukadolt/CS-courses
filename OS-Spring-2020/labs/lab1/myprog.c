004286f8c6a3c1b5f28144c7bc8c71581f5e383d
daukadolt <daulet.amirkhanov.official@gmail.com>
2020-01-29 23:57:51 +0600

    os-spring-20: lab1 minor additions

--- OS-Spring-2020/labs/lab1/myprog.c
+++ OS-Spring-2020/labs/lab1/myprog.c
@@ -2,6 +2,32 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+
+
+int replace_at( int fd, off_t offset, char *s, int n ) {
+    if(offset < 0) {
+        printf("Error: offset cannot be negative\n");
+        return -1;
+    }
+    off_t fileSize = lseek( fd, 0, SEEK_END );
+    if(offset > fileSize) {
+        printf("Error: offset went past the filesize. Maximum offset = %lld\n", fileSize);
+        return -1;
+    }
+    off_t newOffset = lseek( fd, offset, SEEK_SET );
+    if(newOffset == -1) {
+        printf("Error: unknown error when trying to lseek\n");
+        return -1;
+    }
+    ssize_t bytesWritten = write( fd, s, n );
+    if(bytesWritten == -1) {
+        printf("Error: could not perform write to the file\n");
+        return -1;
+    }
+    return (int) lseek( fd, 0, SEEK_CUR );
+}
+
 
 int main(int argc, char **argv) {
     if(argc < 1 || argc < 4 || argc > 4) {
@@ -11,13 +37,12 @@ int main(int argc, char **argv) {
     char *filename = argv[1];
     off_t offset = atoi(argv[2]);
     char *string = argv[3];
-    int fd = open(filename, O_RDONLY);
+    int fd = open(filename, O_WRONLY);
     if(fd == -1) {
         printf("Error: file could not be found\n");
         exit(1);
     }
     printf("File opened successfully. File Descriptor = %d\n", fd);
-    offset = lseek(fd, offset, SEEK_SET);
-    printf("offset = %lld\n", offset);
+    int newOffset = replace_at(fd, offset, string, strlen(string));
     return 0;
 }
